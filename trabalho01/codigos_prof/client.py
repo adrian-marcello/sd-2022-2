3fb93b3136316d8a32eba4f977565adf38646966
adrian-marcello <adrianmarcellodias@gmail.com>
2023-03-16 10:42:20 -0300

    trabalho socket server client

--- trabalho01/codigos_prof/client.py
+++ trabalho01/codigos_prof/client.py
@@ -1,16 +1,14 @@
-#cliente do socket
+#client.py
+
+#!/usr/bin/python                      # This is client.py file
 
 import socket                          # Import socket module
 
 s = socket.socket()                    # Create a socket object
 host = socket.gethostname()            # Get local machine name
-port = 1234                           # Reserve a port for your service.
+port = 12345                           # Reserve a port for your service.
 
 s.connect((host, port))
-msg = ""
-while (msg != "SAIR"):
-    msg = input()
-    s.send(msg.encode())
-    data = s.recv(1024)
-    print("> SERVIDOR: "+data.decode())
+data = s.recv(1024)
+print(data.decode())
 s.close()                              # Close the socket when done
